96bdb25d0ea7613fc928c8a7c3de46b87d1a5aa8
jlucas-as <jlucas_as@hotmail.com>
2026-08-04 19:30:16 -0300

    Google Tag Manager

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -7,7 +7,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WR22F9RM');</script>
+    })(window,document,'script','dataLayer','GTM-WS4RL6LD');</script>
     <!-- End Google Tag Manager -->
 
     <meta charset="utf-8" />
@@ -54,7 +54,7 @@
 
 <body class="{{ !empty($meta['landing']) ? 'landing-page' : '' }}">
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR22F9RM"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WS4RL6LD"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     
